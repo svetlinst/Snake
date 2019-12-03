6997312886ef99f96a5d80a2a2f0632220c0f068
Svetlin Stoyanov <svetlinst@gmail.com>
2019-12-03 21:35:24 +0200

    commit

---
+++ SimpleSnake/Core/FoodFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleSnake.Core
+{
+    using SimpleSnake.GameObjects;
+    using SimpleSnake.Utilities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class FoodFactory
+    {
+        private static Random Random;
+
+        public static Food GenerateRandomFood(int boardWidth, int boardHeight)
+        {
+            var randX = Random.Next(1, boardWidth-1);
+            var randY = Random.Next(1, boardHeight-1);
+            Coordinate foodCoordinate = new Coordinate(randX,randY);
+
+            List<Type> foodTypes = typeof(StartUp)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(Food)))
+                .ToList();
+
+            Type foodType = foodTypes[Random.Next(0, foodTypes.Count)];
+
+            return Activator.CreateInstance(foodType, foodCoordinate) as Food;
+        }
+
+        static FoodFactory()
+        {
+            Random = new Random();
+        }
+    }
+}
---
+++ SimpleSnake/GameObjects/Snake.cs
@@ -0,0 +1,73 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    using SimpleSnake.Enums;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Snake
+    {
+        private List<Coordinate> snakeBody;
+        public Direction CurrentDirection { get; set; }
+        public Coordinate Head { get => snakeBody.Last(); }
+
+        public Snake()
+        {
+            this.snakeBody = new List<Coordinate>();
+            this.InitializeDefaultSnake();
+            this.CurrentDirection = Direction.Right;
+        }
+
+
+        private void InitializeDefaultSnake()
+        {
+            int x = 5;
+            int y = 6;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                this.snakeBody.Add(new Coordinate(x++, y));
+            }
+        }
+
+        private Coordinate CalculateNewCoordinate(Coordinate newCoordinate)
+        {
+            switch (this.CurrentDirection)
+            {
+                case Direction.Right:
+                    newCoordinate.CoordinateX += 1;
+                    break;
+                case Direction.Left:
+                    newCoordinate.CoordinateX -= 1;
+                    break;
+                case Direction.Down:
+                    newCoordinate.CoordinateY += 1;
+                    break;
+                case Direction.Up:
+                    newCoordinate.CoordinateY -= 1;
+                    break;
+                default:
+                    break;
+            }
+            return newCoordinate;
+        }
+
+        public void Coordinate()
+        {
+            Coordinate currentHead = this.snakeBody.Last();
+
+            Coordinate newHeadCoordinate = this.CalculateNewCoordinate(new Coordinate(currentHead.CoordinateX, currentHead.CoordinateY));
+
+            this.snakeBody.Add(newHeadCoordinate);
+            this.snakeBody.RemoveAt(0);
+        }
+
+        public void Eat(Food food)
+        {
+            for (int i = 0; i < food.FoodPoints; i++)
+            {
+                Coordinate coordinate = new Coordinate(this.Head.CoordinateX, this.Head.CoordinateY);
+                this.snakeBody.Add(this.CalculateNewCoordinate(coordinate));
+            }
+        }
+    }
+}
